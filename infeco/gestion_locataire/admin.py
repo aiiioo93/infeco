5de42bddbcab948f80d2482af4fbe1d30386cb04
aiiioo93 <smbbdrame@gmail.com>
2023-03-27 07:02:09 +0200

    Creation de l'administrateur, id: admin mdp:Lalaland@93, ajout des fonction la partie admin et changement de la langue

--- infeco/gestion_locataire/admin.py
+++ infeco/gestion_locataire/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Appartement
+from .models import Locataire
+from .models import EtatDesLieux
+from .models import Paiement
+from .models import QuittanceLoyer
+# Register your models here
+admin.site.register(Appartement)
+admin.site.register(Locataire)
+admin.site.register(EtatDesLieux)
+admin.site.register(Paiement)
+admin.site.register(QuittanceLoyer)
