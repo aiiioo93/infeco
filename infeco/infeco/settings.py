8b0f985e006be9842477facf3d74c8884ffb09bc
aiiioo93 <smbbdrame@gmail.com>
2023-03-30 09:29:21 +0200

    installation heroku, creation du fichier requirement, Procfile  et .gitignore

--- infeco/infeco/settings.py
+++ infeco/infeco/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +27,7 @@ SECRET_KEY = 'django-insecure-lqv%)%&6)1!ya6$mk1gz&^^*5on290&8h*+obc6uib*^#wv*-q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ad-infeco-django.herokuapp.com']
 
 
 # Application definition
@@ -116,6 +118,7 @@ USE_L10N = True
 USE_TZ = True
 
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
@@ -125,3 +128,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals()) 
