45a38756c2454e0b93d9bbab871de2a2db8ee67f
aiiioo93 <smbbdrame@gmail.com>
2023-04-02 00:20:00 +0200

    ok

--- infeco/infeco/settings.py
+++ infeco/infeco/settings.py
@@ -9,11 +9,13 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
-
 import os
-import django_heroku
+
 from pathlib import Path
+
+
+import django_heroku
+
 
 
 
@@ -132,4 +134,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 django_heroku.settings(locals()) 
---
+++ infeco/infeco/views.py
@@ -0,0 +1,11 @@
+from django.contrib.auth import login, authenticate
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+    return render(request, 'login.html')
